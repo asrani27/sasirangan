6949a40a250cbe00f24cf25b37fa97a427edfb00
asrani <asrani.27@gmail.com>
2022-11-28 14:12:51 +0800

    f

--- app/Http/Controllers/FrontEndController.php
+++ app/Http/Controllers/FrontEndController.php
@@ -15,59 +15,60 @@ class FrontEndController extends Controller
 {
     public function beranda()
     {
-        $berita = Berita::orderBy('id','DESC')->limit(4)->get();
+        $berita = Berita::orderBy('id', 'DESC')->limit(4)->get();
         $slider = Slider::orderBy('id', 'DESC')->get();
-        return view('frontend.beranda',compact('berita','slider'));
+        return view('frontend.beranda', compact('berita', 'slider'));
     }
 
     public function info_harga()
     {
         $data = [];
         $pasar = Pasar::get();
-        return view('frontend.info_harga',compact('data','pasar'));
+        return view('frontend.info_harga', compact('data', 'pasar'));
     }
-    
+
     public function info_harga_search()
     {
         $pasar_id = request()->get('pasar_id');
         $tanggal = request()->get('tanggal');
         $tanggal_sekarang = Carbon::parse($tanggal);
         $tanggal_kemarin = Carbon::parse($tanggal)->subDay();
-        
+
         $bahan = Pasar::find($pasar_id)->bahan;
-        $data = $bahan->map(function($item)use($tanggal_sekarang, $tanggal_kemarin, $pasar_id){
-            $item->harga_terkini = $item->harga->where('tanggal', $tanggal_sekarang->format('Y-m-d'))->where('pasar_id', $pasar_id)->first();
-            $item->harga_kemarin = $item->harga->where('tanggal', $tanggal_kemarin->format('Y-m-d'))->where('pasar_id', $pasar_id)->first();
-            if($item->harga_terkini == null){
+        //dd($bahan, $pasar_id);
+        $data = $bahan->map(function ($item) use ($tanggal_sekarang, $tanggal_kemarin, $pasar_id) {
+            $item->harga_terkini = Harga::where('bahan_id', $item->id)->where('tanggal', $tanggal_sekarang->format('Y-m-d'))->where('pasar_id', $pasar_id)->first();
+            $item->harga_kemarin = Harga::where('bahan_id', $item->id)->where('tanggal', $tanggal_kemarin->format('Y-m-d'))->where('pasar_id', $pasar_id)->first();
+            if ($item->harga_terkini == null) {
                 $item->harga_terkini = 0;
-            }else{
+            } else {
                 $item->harga_terkini = $item->harga_terkini->harga;
             }
-            if($item->harga_kemarin == null){
+            if ($item->harga_kemarin == null) {
                 $item->harga_kemarin = 0;
-            }else{
+            } else {
                 $item->harga_kemarin = $item->harga_kemarin->harga;
             }
 
-            if($item->harga_terkini == $item->harga_kemarin){
+            if ($item->harga_terkini == $item->harga_kemarin) {
                 $item->perubahan = 0;
-            }elseif($item->harga_terkini > $item->harga_kemarin){
+            } elseif ($item->harga_terkini > $item->harga_kemarin) {
                 $item->perubahan = $item->harga_terkini - $item->harga_kemarin;
-            }elseif($item->harga_terkini < $item->harga_kemarin){
+            } elseif ($item->harga_terkini < $item->harga_kemarin) {
                 $item->perubahan = $item->harga_terkini - $item->harga_kemarin;
             }
             return $item;
         });
-        
+
         $pasar = Pasar::get();
-        return view('frontend.info_harga',compact('data','pasar','pasar_id', 'tanggal'));
+        return view('frontend.info_harga', compact('data', 'pasar', 'pasar_id', 'tanggal'));
     }
 
     public function info_stok()
     {
         $data = [];
         $pasar = Pasar::get();
-        return view('frontend.info_stok',compact('data','pasar'));
+        return view('frontend.info_stok', compact('data', 'pasar'));
     }
 
     public function info_stok_search()
@@ -76,31 +77,31 @@ class FrontEndController extends Controller
         $tanggal = request()->get('tanggal');
         $tanggal_sekarang = Carbon::parse($tanggal);
         $bulan_lalu = Carbon::parse($tanggal)->subMonth()->endOfMonth();
-        
+
         $bahan = Pasar::find($pasar_id)->bahan;
-        $data = $bahan->map(function($item)use($tanggal_sekarang, $bulan_lalu, $pasar_id){
+        $data = $bahan->map(function ($item) use ($tanggal_sekarang, $bulan_lalu, $pasar_id) {
             $item->stok_terkini = $item->stok->where('tanggal', $tanggal_sekarang->format('Y-m-d'))->where('pasar_id', $pasar_id)->first();
             $item->bulan_lalu = $item->stok->where('tanggal', $bulan_lalu->format('Y-m-d'))->where('pasar_id', $pasar_id)->first();
-            
-            if($item->stok_terkini == null){
+
+            if ($item->stok_terkini == null) {
                 $item->stok_terkini = 0;
-            }else{
+            } else {
                 $item->stok_terkini = $item->stok_terkini->stok;
             }
-            if($item->bulan_lalu == null){
+            if ($item->bulan_lalu == null) {
                 $item->bulan_lalu = 0;
-            }else{
+            } else {
                 $item->bulan_lalu = $item->bulan_lalu->stok;
             }
 
-            if($item->stok_terkini == $item->bulan_lalu){
+            if ($item->stok_terkini == $item->bulan_lalu) {
                 $item->perubahan = 0;
-            }elseif($item->stok_terkini > $item->bulan_lalu){
+            } elseif ($item->stok_terkini > $item->bulan_lalu) {
                 $item->perubahan = $item->stok_terkini - $item->bulan_lalu;
-            }elseif($item->stok_terkini < $item->bulan_lalu){
+            } elseif ($item->stok_terkini < $item->bulan_lalu) {
                 $item->perubahan = $item->stok_terkini - $item->bulan_lalu;
             }
-            
+
             // if($item->bulan_lalu == 0){
             //     $item->persen = 100;
             // }elseif($item->stok_terkini - $item->bulan_lalu == 0){
@@ -110,9 +111,9 @@ class FrontEndController extends Controller
             // }
             return $item;
         });
-//        dd($data);
+        //        dd($data);
         $pasar = Pasar::get();
-        return view('frontend.info_stok',compact('data','pasar','pasar_id', 'tanggal'));
+        return view('frontend.info_stok', compact('data', 'pasar', 'pasar_id', 'tanggal'));
     }
 
     public function grafik_harga()
@@ -120,36 +121,36 @@ class FrontEndController extends Controller
         $data = [];
         $pasar = Pasar::get();
         $bahan = Bahan::get();
-        return view('frontend.grafik_harga',compact('data','pasar','bahan'));
+        return view('frontend.grafik_harga', compact('data', 'pasar', 'bahan'));
     }
-    
+
     public function grafik_harga_search()
     {
         $pasar_id   = request()->get('pasar_id');
         $bulan      = request()->get('bulan');
         $tahun      = request()->get('tahun');
         $bahan_id   = request()->get('bahan_id');
-        $start      = Carbon::createFromFormat('m-Y', $bulan.'-'.$tahun)->startOfMonth();
-        $end        = Carbon::createFromFormat('m-Y', $bulan.'-'.$tahun)->endOfMonth();
-        
+        $start      = Carbon::createFromFormat('m-Y', $bulan . '-' . $tahun)->startOfMonth();
+        $end        = Carbon::createFromFormat('m-Y', $bulan . '-' . $tahun)->endOfMonth();
+
         $date = CarbonPeriod::create($start, $end);
-        
-        foreach($date as $d){
+
+        foreach ($date as $d) {
             $dates[] = $d->format('Y-m-d');
         }
 
         $data['tanggal'] = $dates;
 
         $graph = Harga::where('pasar_id', $pasar_id)->where('bahan_id', $bahan_id)
-        ->whereMonth('tanggal', $bulan)->whereYear('tanggal', $tahun)->get();
+            ->whereMonth('tanggal', $bulan)->whereYear('tanggal', $tahun)->get();
 
         $rgbColor = array();
-            foreach(array('r', 'g', 'b') as $color){
-                $rgbColor[$color] = mt_rand(0, 255);
-            }
-        
-        foreach($dates as $item){
-            $dataAngka[] = $graph->where('tanggal', $item)->first() == null ? 0:$graph->where('tanggal', $item)->first()->harga;
+        foreach (array('r', 'g', 'b') as $color) {
+            $rgbColor[$color] = mt_rand(0, 255);
+        }
+
+        foreach ($dates as $item) {
+            $dataAngka[] = $graph->where('tanggal', $item)->first() == null ? 0 : $graph->where('tanggal', $item)->first()->harga;
         }
 
         $dataset[] = [
@@ -157,24 +158,24 @@ class FrontEndController extends Controller
             'fill' => false,
             'data' => $dataAngka,
             'borderColor' => [
-                'rgba('.$rgbColor['r'].', '.$rgbColor['g'].', '.$rgbColor['b'].')'
+                'rgba(' . $rgbColor['r'] . ', ' . $rgbColor['g'] . ', ' . $rgbColor['b'] . ')'
             ],
-            'borderWidth'=> 2,
+            'borderWidth' => 2,
         ];
-        
+
         $bahan = Bahan::get();
-        
+
         $pasar = Pasar::get();
-        return view('frontend.grafik_harga',compact('data','pasar', 'pasar_id', 'bulan', 'tahun','bahan','bahan_id','dataset'));
+        return view('frontend.grafik_harga', compact('data', 'pasar', 'pasar_id', 'bulan', 'tahun', 'bahan', 'bahan_id', 'dataset'));
     }
 
     public function grafik_stok()
     {
         $data = [];
         $pasar = Pasar::get();
-        return view('frontend.grafik_stok',compact('data','pasar'));
+        return view('frontend.grafik_stok', compact('data', 'pasar'));
     }
-    
+
     public function grafik_stok_search()
     {
         $pasar_id = request()->get('pasar_id');
@@ -182,7 +183,7 @@ class FrontEndController extends Controller
         $tahun = request()->get('tahun');
         // $start = Carbon::createFromFormat('m-Y', $bulan.'-'.$tahun)->startOfMonth();
         // $end = Carbon::createFromFormat('m-Y', $bulan.'-'.$tahun)->endOfMonth();
-        
+
         // $date = CarbonPeriod::create($start, $end);
         // $dates = [];
         // foreach($date as $d){
@@ -190,13 +191,13 @@ class FrontEndController extends Controller
         // }
 
         // $data['tanggal'] = $dates;
-        
+
         $pasar = Pasar::get();
         $data = [
             'name' => 'asrani',
         ];
         //dd($data);
-        return view('frontend.grafik_stok',compact('data','pasar', 'pasar_id', 'tahun'));
+        return view('frontend.grafik_stok', compact('data', 'pasar', 'pasar_id', 'tahun'));
     }
 
     public function login()
@@ -206,7 +207,7 @@ class FrontEndController extends Controller
 
     public function artikel()
     {
-        $berita = Berita::orderBy('id','DESC')->paginate(5);
-        return view('frontend.artikel',compact('berita'));
+        $berita = Berita::orderBy('id', 'DESC')->paginate(5);
+        return view('frontend.artikel', compact('berita'));
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,21 +1,21 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-Route::middleware('visit')->group(function() {
-    Route::get('/', 'FrontEndController@beranda');
-    Route::get('/info-harga', 'FrontEndController@info_harga');
-    Route::get('/info-harga/search', 'FrontEndController@info_harga_search');
-    Route::get('/info-stok', 'FrontEndController@info_stok');
-    Route::get('/info-stok/search', 'FrontEndController@info_stok_search');
-    Route::get('/grafik', 'FrontEndController@grafik');
-    Route::get('/artikel', 'FrontEndController@artikel');
-    Route::get('/grafik/harga', 'FrontEndController@grafik_harga');
-    Route::get('/grafik/stok', 'FrontEndController@grafik_stok');
-    Route::get('/grafik/harga/search', 'FrontEndController@grafik_harga_search');
-    Route::get('/grafik/stok/search', 'FrontEndController@grafik_stok_search');
-    Route::get('/login', 'FrontEndController@login')->name('login');
-    Route::post('/login', 'LoginController@login');
-});
+//Route::middleware('visit')->group(function() {
+Route::get('/', 'FrontEndController@beranda');
+Route::get('/info-harga', 'FrontEndController@info_harga');
+Route::get('/info-harga/search', 'FrontEndController@info_harga_search');
+Route::get('/info-stok', 'FrontEndController@info_stok');
+Route::get('/info-stok/search', 'FrontEndController@info_stok_search');
+Route::get('/grafik', 'FrontEndController@grafik');
+Route::get('/artikel', 'FrontEndController@artikel');
+Route::get('/grafik/harga', 'FrontEndController@grafik_harga');
+Route::get('/grafik/stok', 'FrontEndController@grafik_stok');
+Route::get('/grafik/harga/search', 'FrontEndController@grafik_harga_search');
+Route::get('/grafik/stok/search', 'FrontEndController@grafik_stok_search');
+Route::get('/login', 'FrontEndController@login')->name('login');
+Route::post('/login', 'LoginController@login');
+//});
 
 Route::group(['middleware' => ['auth', 'role:superadmin|admin']], function () {
     Route::get('/home', 'HomeController@index');
@@ -26,42 +26,42 @@ Route::group(['middleware' => ['auth', 'role:superadmin|admin']], function () {
     Route::get('/informasi/kategori/edit/{id}', 'KategoriController@edit');
     Route::post('/informasi/kategori/edit/{id}', 'KategoriController@update');
     Route::get('/informasi/kategori/delete/{id}', 'KategoriController@delete');
-    
+
     Route::get('/informasi/berita', 'BeritaController@index');
     Route::get('/informasi/berita/add', 'BeritaController@create');
     Route::post('/informasi/berita/add', 'BeritaController@store');
     Route::get('/informasi/berita/edit/{id}', 'BeritaController@edit');
     Route::post('/informasi/berita/edit/{id}', 'BeritaController@update');
     Route::get('/informasi/berita/delete/{id}', 'BeritaController@delete');
-    
+
     Route::get('/data/pengguna', 'UserController@index');
     Route::get('/data/pengguna/add', 'UserController@create');
     Route::post('/data/pengguna/add', 'UserController@store');
     Route::get('/data/pengguna/edit/{id}', 'UserController@edit');
     Route::post('/data/pengguna/edit/{id}', 'UserController@update');
     Route::get('/data/pengguna/delete/{id}', 'UserController@delete');
-    
+
     Route::get('/data/satuan', 'SatuanController@index');
     Route::get('/data/satuan/add', 'SatuanController@create');
     Route::post('/data/satuan/add', 'SatuanController@store');
     Route::get('/data/satuan/edit/{id}', 'SatuanController@edit');
     Route::post('/data/satuan/edit/{id}', 'SatuanController@update');
     Route::get('/data/satuan/delete/{id}', 'SatuanController@delete');
-    
+
     Route::get('/data/kelompok', 'KelompokController@index');
     Route::get('/data/kelompok/add', 'KelompokController@create');
     Route::post('/data/kelompok/add', 'KelompokController@store');
     Route::get('/data/kelompok/edit/{id}', 'KelompokController@edit');
     Route::post('/data/kelompok/edit/{id}', 'KelompokController@update');
     Route::get('/data/kelompok/delete/{id}', 'KelompokController@delete');
-    
+
     Route::get('/data/pasar', 'PasarController@index');
     Route::get('/data/pasar/add', 'PasarController@create');
     Route::post('/data/pasar/add', 'PasarController@store');
     Route::get('/data/pasar/edit/{id}', 'PasarController@edit');
     Route::post('/data/pasar/edit/{id}', 'PasarController@update');
     Route::get('/data/pasar/delete/{id}', 'PasarController@delete');
-    
+
     Route::get('/data/bahan', 'BahanController@index');
     Route::get('/data/bahan/add', 'BahanController@create');
     Route::post('/data/bahan/add', 'BahanController@store');
@@ -72,13 +72,13 @@ Route::group(['middleware' => ['auth', 'role:superadmin|admin']], function () {
     Route::get('/data/gantipassword', 'GantiPassController@index');
     Route::post('/data/gantipassword', 'GantiPassController@change');
 
-    
+
     Route::get('/input/harga', 'HargaController@index');
     Route::post('/input/harga/update', 'HargaController@updateService');
     Route::get('/input/harga/fulldate/{id}', 'HargaController@fullDate');
     Route::get('/input/harga/{id}', 'HargaController@pasar');
     Route::get('/input/harga/bahan/sekarang', 'HargaController@storeHargaBahan');
-    
+
     Route::get('/input/stok', 'StokController@index');
     Route::get('/input/stok/month', 'StokController@month');
     Route::post('/input/stok/update', 'StokController@updateService');
@@ -91,7 +91,7 @@ Route::group(['middleware' => ['auth', 'role:superadmin|admin']], function () {
     Route::post('/informasi/slider/edit/{id}', 'SliderController@update');
     Route::get('/informasi/slider/delete/{id}', 'SliderController@delete');
 
-    
+
     Route::get('/report/harga/rata-rata/harian', 'ReportController@harian');
     Route::get('/report/harga/rata-rata/harian/search', 'ReportController@searchHarian');
     Route::get('/report/harga/rata-rata/bulanan', 'ReportController@bulanan');
@@ -99,7 +99,7 @@ Route::group(['middleware' => ['auth', 'role:superadmin|admin']], function () {
     Route::get('/report/stok/bulanan', 'ReportController@stok');
     Route::get('/report/stok/grafik', 'ReportController@grafik_stok');
     Route::get('/report/stok/grafik/search', 'ReportController@search_grafik_stok');
-}); 
+});
 
 Route::get('/logout', function () {
     Auth::logout();
