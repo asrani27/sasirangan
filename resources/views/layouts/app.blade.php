b7930e86466e89861f4ecd6eb1d25cbbf21e6250
asrani <asrani.27@gmail.com>
2023-11-03 09:38:46 +0800

    f

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,6 +6,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
         <title>DEDIKASI BAIMAN</title>
+        <link rel="manifest" href="/manifest.json">
         <meta content="Admin Dashboard" name="description" />
         <meta content="Themesbrand" name="author" />
         @stack('meta')
