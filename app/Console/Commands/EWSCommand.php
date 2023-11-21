28c193e42c8edc60e077ae604396a8cdb79dd434
asrani <asrani.27@gmail.com>
2023-11-21 19:21:05 +0800

    f

--- app/Console/Commands/EWSCommand.php
+++ app/Console/Commands/EWSCommand.php
@@ -2,8 +2,12 @@
 
 namespace App\Console\Commands;
 
+use App\Harga;
 use App\Cronjob;
+use App\Kenaikan;
 use Carbon\Carbon;
+use App\BulanTahun;
+use App\HargaAcuan;
 use Illuminate\Console\Command;
 
 class EWSCommand extends Command
@@ -40,9 +44,64 @@ class EWSCommand extends Command
     public function handle()
     {
         //EWS dan notif ke wa
-        $n = new Cronjob;
-        $n->cronjob = Carbon::now()->format('Y-m-d H:i:s');
-        $n->save();
+        for ($x = 1; $x <= 10; $x++) {
+            $n = new Cronjob;
+            $n->cronjob = Carbon::now()->format('Y-m-d H:i:s');
+            $n->save();
+            $n->delay(5);
+        }
+        $year = Carbon::now()->year;
+        $month = Carbon::now()->month;
+
+        $now = Carbon::now()->format('Y-m-d');
+        $hargaToday = Harga::where('tanggal', $now)->get()->map(function ($item) use ($month, $year, $now) {
+            $bulantahun_id = BulanTahun::where('bulan', $month)->where('tahun', $year)->where('pasar_id', $item->pasar_id)->first();
+            dd($item);
+            if ($bulantahun_id == null) {
+            } else {
+                $hargaAcuan = HargaAcuan::where('bulan_tahun_id', $bulantahun_id->id)->where('pasar_id', $item->pasar_id)->where('bahan_id', $item->bahan_id)->first();
+
+                if ($hargaAcuan == null) {
+                } else {
+                    //jika acuan 0 kasih notif
+                    if ($hargaAcuan->harga == 0) {
+                        toastr()->error(' Harga Acuan Tidak Boleh 0');
+                        return back();
+                    } else {
+                        $acuan = $hargaAcuan->harga;
+                        //dd($acuan, $item->harga);
+                        $kenaikan = (($item->harga - $acuan) / $acuan) * 100;
+                        //dd($kenaikan, $now, $item->pasar_id, $item->bahan_id, $acuan, $item->harga);
+                        //simpan jika kenaik lebih dari 5 %
+                        if ($kenaikan > 5) {
+                            $checkKenaikan = Kenaikan::where('tanggal', $now)->where('pasar_id', $item->pasar_id)->where('bahan_id', $item->bahan_id)->first();
+                            if ($checkKenaikan == null) {
+                                //new save
+                                $n = new Kenaikan;
+                                $n->tanggal = $now;
+                                $n->pasar_id = $item->pasar_id;
+                                $n->bahan_id = $item->bahan_id;
+                                $n->acuan = $acuan;
+                                $n->harga = $item->harga;
+                                $n->kenaikan = $kenaikan;
+                                $n->save();
+                            } else {
+                                //update
+                                $u = $checkKenaikan;
+                                $u->harga = $item->harga;
+                                $u->kenaikan = $kenaikan;
+                                $u->save();
+                            }
+                        } else {
+                        }
+                    }
+                }
+            }
+            return $item;
+        });
+
+        // ProcessPodcast::dispatch($podcast)
+        //             ->delay(now()->addMinutes(10));
         $this->info('Berhasil di simpan ' . Carbon::now()->format('Y-m-d H:i:s'));
     }
 }
